c8fd5ee8f66c6d1a46cfd105008e2d86d570eab2
unknown <sibashishbiswas349@gmail.com>
2023-08-15 21:55:55 +0530

    Java Singleton Design Pattern

---
+++ Singleton Design Pattern/src/com/example/demo/ThreadSafeSingleton.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class ThreadSafeSingleton {
+	private static ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton(){}
+
+    public static synchronized ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            instance = new ThreadSafeSingleton();
+        }
+        return instance;
+    }
+}
